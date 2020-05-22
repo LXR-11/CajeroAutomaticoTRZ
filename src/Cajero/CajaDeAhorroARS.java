ca4ab775e78d0f954473e75da2f47151888ef8a1
Agustin-Aguilera <agustin.aguilera424@gmail.com>
2020-05-22 14:04:47 -0300

    Caja de Ahorro en Pesos

--- src/Cajero/CajaDeAhorroARS.java
+++ src/Cajero/CajaDeAhorroARS.java
@@ -1,30 +1,39 @@
 package Cajero;
 
-public class CajaDeAhorroARS extends Cuenta implements Operacion{
+public class CajaDeAhorroARS extends Cuenta implements Operacion {
 	public CajaDeAhorroARS(double saldo, String alias) throws Exception {
-		super(saldo,alias);
+		super(saldo, alias);
 	}
 
-	
 	public void retirarEfectivo(int valor) {
-		if(valor>this.saldo) {
-			this.saldo=-valor;
-			System.out.println("Se extrajo correctamente $"+valor);
+		if (saldoSuficiente(valor)) {
+			this.saldo = -valor;
+		} else {
+			System.out.println("No se pudo extraer. No posee suficiente saldo en su cuenta");
 		}
-		else {
-			System.out.println("No se pudo extraer. Valor invalido");
-		}
+	}
+
+	public boolean saldoSuficiente(int saldoAretirar) {
+		return saldoAretirar >= this.saldo;
 	}
 	
-	public void comprarDolares(int valor) {
-		if(valor>=this.saldo/this.valorDelDolar*valor) {
-			//HAY QUE USAR DOS SALDOS? UNO EN PESOS Y OTRO EN ARS
-			//O HACER UNA TRANSFERENCIA A LA OTRA CUENTA SI POSEE?
+	public void comprarDolares(int valor, Cliente cliente) {
+
+		if (cliente.cajaDelClienteUSD != null) {
+			if (valor >= this.saldo / this.valorDelDolar * valor) {
+				cliente.cajaDelClienteUSD.depositar(valor);
+			}
+			else {System.out.println("Saldo insuficiente para realizar esta operacion");}
+			
+			
+		} else {
+			System.out.println("Usted no posee una Caja de Ahorro en USD");
 		}
+
 	}
+
 	public void transferir(String alias, int valor) {
-		
+
 	}
 
 }
-

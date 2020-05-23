6a55d2986981c4ad8901553961f717eb3bc2a36d
Agustin-Aguilera <agustin.aguilera424@gmail.com>
2020-05-23 19:01:38 -0300

    Transferencia

--- src/Cajero/CuentaCorriente.java
+++ src/Cajero/CuentaCorriente.java
@@ -11,7 +11,7 @@ public class CuentaCorriente extends Cuenta implements Operacion{
 	
 	public void retirarEfectivo(int valor) {}
 	public void comprarDolares(int valor) {}
-	public void transferir(String alias, int valor) {}
+	public void transferir(Cuenta cuentaDestino, int valor) {}
 
 	@Override
 	public void comprarDolares(int valor, Cliente cliente) {
